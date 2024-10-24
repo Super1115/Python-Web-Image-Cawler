6baa5f614349a39a5103a3a7f551e068009465ed
= <chuangsuper@gmail.com>
2024-10-25 01:19:53 +0800

    added settings and DL

--- app.py
+++ app.py
@@ -1,16 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
-requestHeaders = {'user-agent': 'Penguinly/1.0.0'}
+import json
+import os
+
+# read Data From settings
+settingsJsonFile = open("settings.json") #settingsJsonFile location
+settingsJsonData = json.load(settingsJsonFile) 
+settingsJsonFile.close()
+print("settings : ")
+print(settingsJsonData)
+
+#Http request
+requestHeaders = {'user-agent': settingsJsonData["user-agent"]}
+print("Http Request Headers:")
+print(requestHeaders)
 
 def getDataInHtml(url):
     r = requests.get(url)
     print("Http Status Code : "+ str(r.status_code))
-    return BeautifulSoup(r.text,'html.parser')
+    if r.status_code == 200:
+        return BeautifulSoup(r.text,'html.parser')
+    else : 
+        print("Failed! can't conect to URL")
+        exit()
 
 def DlAllImgFromHtml(html):
+    returnArray = []
     for i in html.find_all("img"):
-        print(i["src"])
+        returnArray.append(i["src"])
+        print("Found Image : "+str(i["src"]))
+    return returnArray
+
+def DlImg(url):
+    if os.path.isdir(settingsJsonData["downloadDirectory"]) != True:
+        os.mkdir(settingsJsonData["downloadDirectory"]) 
+
+    with open(f"{settingsJsonData["downloadDirectory"]}/{url.split("/")[-1]}","wb") as file:
+        file.write(requests.get(url).content)
+
+def DlArrayImg(urlArray):
+    for x in urlArray:
+        DlImg(x)
 
 print("Check Config Folder For Settings")
+
+#test
+
 a = input("url")
-DlAllImgFromHtml(getDataInHtml(a))
+DlArrayImg(DlAllImgFromHtml(getDataInHtml(a)))
